2a77be050fa90945b06246636761ae5e7101ce4b
Matthew James <mjames4@woolworths.com.au>
2021-07-16 10:08:26 +1000

    Start hydrating todo list on load

--- lib/synced_todo_list_notifier.dart
+++ lib/synced_todo_list_notifier.dart
@@ -5,8 +5,12 @@ import 'package:todo_app_embbedv2/domain/todo_list.dart';
 
 class SyncedTodoListNotifier extends StateNotifier<TodoList> {
   final SyncStore<TodoList> _syncStore;
-  SyncedTodoListNotifier(SyncStore<TodoList> syncStore)
-      : _syncStore = syncStore, super(new TodoList());
+
+  SyncedTodoListNotifier(SyncStore<TodoList> syncStore) :
+        _syncStore = syncStore,
+        super(new TodoList()) {
+    _hydrateTodoList();
+  }
 
   Future add(TodoItem item) async {
     state = await _withStateChanged((s) => s.add(item));
@@ -28,5 +32,14 @@ class SyncedTodoListNotifier extends StateNotifier<TodoList> {
     final changedState = map(state);
     await _syncStore.upsert(changedState);
     return changedState;
+  }
+
+  Future _hydrateTodoList() async {
+    final List<dynamic> data = await _syncStore.getMany();
+    if (data?.isEmpty ?? true) {
+      return;
+    }
+
+    state = TodoList.parse(data[0]);
   }
 }
